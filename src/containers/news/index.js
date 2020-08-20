1d2806e02509d488e3de5e946a0f8f57c802632d
BardAlexandrovskiy <alexandr8558@gmail.com>
2020-08-20 12:12:12 +0300

    bugs fixed

--- src/containers/news/index.js
+++ src/containers/news/index.js
@@ -1,33 +1,34 @@
-import React from 'react';
-import { NewsContainer, Title } from './styles';
-import NewsItem from '../../components/NewsItem';
-import Preloader from '../../components/Preloader';
-import { connect } from 'react-redux';
-import { hideError, showError } from '../../actions';
-import Error from '../../components/Error';
+import React from "react";
+import { NewsContainer, Title } from "./styles";
+import NewsItem from "../../components/NewsItem";
+import Preloader from "../../components/Preloader";
+import { connect } from "react-redux";
+import { hideError, showError } from "../../actions";
+import Error from "../../components/Error";
 
 class News extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       arrNews: [],
-      preloader: false
+      preloader: false,
     };
   }
 
   componentDidMount() {
     const { showError } = this.props;
     this.setState({ preloader: true });
-    fetch('https://api.nytimes.com/svc/news/v3/content/all/all.json?api-key=GWNR75tapwV4yS2UbyV16BHepPcUTcZW')
-      .then(res => {
+    fetch(
+      "https://api.nytimes.com/svc/news/v3/content/all/all.json?api-key=GWNR75tapwV4yS2UbyV16BHepPcUTcZW"
+    )
+      .then((res) => {
         if (res.status === 200) {
           return res.json();
         }
         throw new Error(res.status);
       })
-      .then(newsObj => {
-        console.log(newsObj.results[0])
-        return this.setState({ arrNews: newsObj.results })
+      .then((newsObj) => {
+        return this.setState({ arrNews: newsObj.results });
       })
       .catch(() => showError())
       .finally(() => this.setState({ preloader: false }));
@@ -47,36 +48,36 @@ class News extends React.Component {
         {preloader && <Preloader />}
         <Title>Latest news:</Title>
         {arrNews.map((item, index) => {
-          console.log(item)
           let img;
-
           if (item.multimedia) {
-            img = item.multimedia[2].url
+            if (item.multimedia[2]) {
+              img = item.multimedia[2].url;
+            }
           } else img = null;
 
           return (
-            < NewsItem
+            <NewsItem
               key={index}
               description={item.abstract}
               title={item.title}
               url={item.url}
               img={img}
             />
-          )
+          );
         })}
       </NewsContainer>
     );
   }
 }
 
-const mapStateToProps = state => {
+const mapStateToProps = (state) => {
   return { error: state.news.error };
 };
 
-const mapDispatchToProps = dispatch => {
+const mapDispatchToProps = (dispatch) => {
   return {
     showError: () => dispatch(showError()),
-    hideError: () => dispatch(hideError())
+    hideError: () => dispatch(hideError()),
   };
 };
 
